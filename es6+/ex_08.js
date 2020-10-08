a5bf83d451068cfc8e866b14d9ee1f4086004e16
muNeves3 <murilogrilosn@gmail.com>
2020-10-08 19:09:52 -0300

    :books: exercises 8

--- es6+/ex_08.js
+++ es6+/ex_08.js
@@ -1,5 +1,10 @@
-import ClasseUsuario, { idade as IdadeUsuario} from './functions.js';
+import ClasseUsuario, { idade, idade as IdadeUsuario} from './functions.js';
 
+//1.1
 ClasseUsuario.info();
 
+//1.2   
+console.log(idade);
+
+//1.3
 console.log(IdadeUsuario);
